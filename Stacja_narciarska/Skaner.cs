bbe6c912773b6a1f7b8345ed95f55d415f8e3c00
Kacperel <kacper.l@onet.pl>
2021-12-06 21:10:42 +0100

    poczatki_v2

--- Stacja_narciarska/Skaner.cs
+++ Stacja_narciarska/Skaner.cs
@@ -16,5 +16,10 @@ namespace Stacja_narciarska
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
