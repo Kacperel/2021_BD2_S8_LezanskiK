55959200c7eb0a01448cb02f237bcc0f3f736d67
Kacperel <kacper.l@onet.pl>
2022-01-19 22:19:17 +0100

    alfa

--- Stacja_narciarska/zmianaCen.Designer.cs
+++ Stacja_narciarska/zmianaCen.Designer.cs
@@ -29,33 +29,33 @@ namespace Stacja_narciarska
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lista_bilety = new System.Windows.Forms.ComboBox();
+            this.cena_bilet = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.aktualizuj = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.bilet_update = new System.Windows.Forms.Button();
+            this.karnet_update = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.cena_karnet = new System.Windows.Forms.TextBox();
+            this.lista_karnety = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // lista_bilety
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(327, 85);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 0;
+            this.lista_bilety.FormattingEnabled = true;
+            this.lista_bilety.Location = new System.Drawing.Point(327, 85);
+            this.lista_bilety.Name = "lista_bilety";
+            this.lista_bilety.Size = new System.Drawing.Size(121, 21);
+            this.lista_bilety.TabIndex = 0;
             // 
-            // textBox1
+            // cena_bilet
             // 
-            this.textBox1.Location = new System.Drawing.Point(327, 149);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.cena_bilet.Location = new System.Drawing.Point(327, 149);
+            this.cena_bilet.Name = "cena_bilet";
+            this.cena_bilet.Size = new System.Drawing.Size(100, 20);
+            this.cena_bilet.TabIndex = 1;
+            this.cena_bilet.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
@@ -75,23 +75,25 @@ namespace Stacja_narciarska
             this.label2.TabIndex = 3;
             this.label2.Text = "Wprowadz nowa cene";
             // 
-            // aktualizuj
+            // bilet_update
             // 
-            this.aktualizuj.Location = new System.Drawing.Point(337, 185);
-            this.aktualizuj.Name = "aktualizuj";
-            this.aktualizuj.Size = new System.Drawing.Size(75, 23);
-            this.aktualizuj.TabIndex = 4;
-            this.aktualizuj.Text = "Aktualizuj";
-            this.aktualizuj.UseVisualStyleBackColor = true;
+            this.bilet_update.Location = new System.Drawing.Point(337, 185);
+            this.bilet_update.Name = "bilet_update";
+            this.bilet_update.Size = new System.Drawing.Size(75, 23);
+            this.bilet_update.TabIndex = 4;
+            this.bilet_update.Text = "Aktualizuj";
+            this.bilet_update.UseVisualStyleBackColor = true;
+            this.bilet_update.Click += new System.EventHandler(this.bilet_update_Click);
             // 
-            // button1
+            // karnet_update
             // 
-            this.button1.Location = new System.Drawing.Point(133, 202);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Aktualizuj";
-            this.button1.UseVisualStyleBackColor = true;
+            this.karnet_update.Location = new System.Drawing.Point(133, 202);
+            this.karnet_update.Name = "karnet_update";
+            this.karnet_update.Size = new System.Drawing.Size(75, 23);
+            this.karnet_update.TabIndex = 9;
+            this.karnet_update.Text = "Aktualizuj";
+            this.karnet_update.UseVisualStyleBackColor = true;
+            this.karnet_update.Click += new System.EventHandler(this.karnet_update_Click);
             // 
             // label3
             // 
@@ -111,35 +113,36 @@ namespace Stacja_narciarska
             this.label4.TabIndex = 7;
             this.label4.Text = "Wybierz karnet";
             // 
-            // textBox2
+            // cena_karnet
             // 
-            this.textBox2.Location = new System.Drawing.Point(123, 166);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 6;
+            this.cena_karnet.Location = new System.Drawing.Point(123, 166);
+            this.cena_karnet.Name = "cena_karnet";
+            this.cena_karnet.Size = new System.Drawing.Size(100, 20);
+            this.cena_karnet.TabIndex = 6;
             // 
-            // comboBox2
+            // lista_karnety
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(123, 102);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 21);
-            this.comboBox2.TabIndex = 5;
+            this.lista_karnety.FormattingEnabled = true;
+            this.lista_karnety.Location = new System.Drawing.Point(123, 102);
+            this.lista_karnety.Name = "lista_karnety";
+            this.lista_karnety.Size = new System.Drawing.Size(121, 21);
+            this.lista_karnety.TabIndex = 5;
+            this.lista_karnety.SelectedIndexChanged += new System.EventHandler(this.lista_karnety_SelectedIndexChanged);
             // 
             // zmianaCen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.karnet_update);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.aktualizuj);
+            this.Controls.Add(this.cena_karnet);
+            this.Controls.Add(this.lista_karnety);
+            this.Controls.Add(this.bilet_update);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cena_bilet);
+            this.Controls.Add(this.lista_bilety);
             this.Name = "zmianaCen";
             this.Size = new System.Drawing.Size(619, 446);
             this.Load += new System.EventHandler(this.zmianaCen_Load);
@@ -150,15 +153,15 @@ namespace Stacja_narciarska
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox lista_bilety;
+        private System.Windows.Forms.TextBox cena_bilet;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button aktualizuj;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button bilet_update;
+        private System.Windows.Forms.Button karnet_update;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox cena_karnet;
+        private System.Windows.Forms.ComboBox lista_karnety;
     }
 }
--- Stacja_narciarska/zmianaCen.cs
+++ Stacja_narciarska/zmianaCen.cs
@@ -7,14 +7,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace Stacja_narciarska
 {
     public partial class zmianaCen : UserControl
     {
-        public zmianaCen()
+       
+public zmianaCen()
         {
             InitializeComponent();
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=5432;Database=Stacja_narciarska;User Id=postgres;Password=rowery;");
+            conn.Open();
+            NpgsqlCommand comm = new NpgsqlCommand();
+            comm.Connection = conn;
+            comm.CommandType = CommandType.Text;
+            comm.CommandText = "SELECT nazwa FROM public.rodzaj_karnetu";
+            NpgsqlDataReader dr = comm.ExecuteReader();
+            if (dr.HasRows)
+            {
+                // DataTable dt = new DataTable();
+                // dt.Load(dr);
+                //comboBox1.DataSource = dt;
+                //comboBox1.Items.Add(dr);
+                while (dr.Read())
+                {
+                    lista_karnety.Items.Add(dr[0].ToString());
+                }
+
+
+
+            }
+            comm.Dispose();
+            conn.Close();
+
+            NpgsqlConnection conn2 = new NpgsqlConnection("Server=localhost;Port=5432;Database=Stacja_narciarska;User Id=postgres;Password=rowery;");
+            conn2.Open();
+            NpgsqlCommand comm2 = new NpgsqlCommand();
+            comm2.Connection = conn2;
+            comm2.CommandType = CommandType.Text;
+            comm2.CommandText = "SELECT nazwa FROM public.rodzaj_biletu";
+            NpgsqlDataReader dr2 = comm2.ExecuteReader();
+            if (dr2.HasRows)
+            {
+                // DataTable dt = new DataTable();
+                // dt.Load(dr);
+                //comboBox1.DataSource = dt;
+                //comboBox1.Items.Add(dr);
+                while (dr2.Read())
+                {
+                    lista_bilety.Items.Add(dr2[0].ToString());
+                }
+
+
+
+            }
+            comm2.Dispose();
+            conn2.Close();
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -25,6 +75,44 @@ namespace Stacja_narciarska
         private void zmianaCen_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void lista_karnety_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void karnet_update_Click(object sender, EventArgs e)
+        {
+
+            NpgsqlConnection conn3 = new NpgsqlConnection("Server=localhost;Port=5432;Database=Stacja_narciarska;User Id=postgres;Password=rowery;");
+            conn3.Open();
+
+            NpgsqlCommand comm3 = new NpgsqlCommand();
+            comm3.Connection = conn3;
+            comm3.CommandType = CommandType.Text;
+            comm3.CommandText = "UPDATE public.cena_karnetu SET cena_karnetu='"+ cena_karnet.Text +"' WHERE rodzaj_karnetu_id_rodzaj_karnetu = (SELECT id_rodzaj_karnetu FROM public.rodzaj_karnetu WHERE nazwa='"+ lista_karnety.Text +"')";
+            comm3.ExecuteNonQuery();
+            MessageBox.Show("Zaktualizowano pomyslnie");
+
+            comm3.Dispose();
+            conn3.Close();
+        }
+
+        private void bilet_update_Click(object sender, EventArgs e)
+        {
+            NpgsqlConnection conn3 = new NpgsqlConnection("Server=localhost;Port=5432;Database=Stacja_narciarska;User Id=postgres;Password=rowery;");
+            conn3.Open();
+
+            NpgsqlCommand comm3 = new NpgsqlCommand();
+            comm3.Connection = conn3;
+            comm3.CommandType = CommandType.Text;
+            comm3.CommandText = "UPDATE public.cena_biletu SET cena_biletu='" + cena_bilet.Text + "' WHERE rodzaj_biletu_id_rodzaj_biletu = (SELECT id_rodzaj_biletu FROM public.rodzaj_biletu WHERE nazwa='" + lista_bilety.Text + "')";
+            comm3.ExecuteNonQuery();
+            MessageBox.Show("Zaktualizowano pomyslnie");
+
+            comm3.Dispose();
+            conn3.Close();
         }
     }
 }
