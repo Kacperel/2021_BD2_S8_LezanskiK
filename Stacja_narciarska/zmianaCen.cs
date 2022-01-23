5adf8ff0225c2fe3f728a743bcef29efa27299c0
Kacperel <kacper.l@onet.pl>
2022-01-23 17:27:03 +0100

    poprawki

--- Stacja_narciarska/zmianaCen.cs
+++ Stacja_narciarska/zmianaCen.cs
@@ -13,8 +13,8 @@ namespace Stacja_narciarska
 {
     public partial class zmianaCen : UserControl
     {
-       
-public zmianaCen()
+
+        public zmianaCen()
         {
             InitializeComponent();
             NpgsqlConnection conn7 = new NpgsqlConnection("Server=localhost;Port=5432;Database=Stacja_narciarska;User Id=postgres;Password=rowery;");
@@ -84,35 +84,64 @@ public zmianaCen()
 
         private void karnet_update_Click(object sender, EventArgs e)
         {
+            if (!string.IsNullOrEmpty(lista_karnety.Text))
+            {
+                if (!string.IsNullOrEmpty(cena_karnet.Text))
+                {
+                    NpgsqlConnection conn3 = new NpgsqlConnection("Server=localhost;Port=5432;Database=Stacja_narciarska;User Id=postgres;Password=rowery;");
+                    conn3.Open();
 
-            NpgsqlConnection conn3 = new NpgsqlConnection("Server=localhost;Port=5432;Database=Stacja_narciarska;User Id=postgres;Password=rowery;");
-            conn3.Open();
+                    NpgsqlCommand comm3 = new NpgsqlCommand();
+                    comm3.Connection = conn3;
+                    comm3.CommandType = CommandType.Text;
+                    comm3.CommandText = "UPDATE public.cena_karnetu SET cena_karnetu='" + cena_karnet.Text + "' WHERE rodzaj_karnetu_id_rodzaj_karnetu = (SELECT id_rodzaj_karnetu FROM public.rodzaj_karnetu WHERE   nazwa='" + lista_karnety.Text + "')";
+                    comm3.ExecuteNonQuery();
+                    MessageBox.Show("Zaktualizowano pomyslnie");
 
-            NpgsqlCommand comm3 = new NpgsqlCommand();
-            comm3.Connection = conn3;
-            comm3.CommandType = CommandType.Text;
-            comm3.CommandText = "UPDATE public.cena_karnetu SET cena_karnetu='"+ cena_karnet.Text +"' WHERE rodzaj_karnetu_id_rodzaj_karnetu = (SELECT id_rodzaj_karnetu FROM public.rodzaj_karnetu WHERE nazwa='"+ lista_karnety.Text +"')";
-            comm3.ExecuteNonQuery();
-            MessageBox.Show("Zaktualizowano pomyslnie");
+                    comm3.Dispose();
+                    conn3.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Nie podano nowej ceny!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nie wybrabo karnetu!");
+            }
 
-            comm3.Dispose();
-            conn3.Close();
         }
 
         private void bilet_update_Click(object sender, EventArgs e)
         {
-            NpgsqlConnection conn4 = new NpgsqlConnection("Server=localhost;Port=5432;Database=Stacja_narciarska;User Id=postgres;Password=rowery;");
-            conn4.Open();
+            if (!string.IsNullOrEmpty(lista_bilety.Text))
+            {
+                if (!string.IsNullOrEmpty(cena_bilet.Text))
+                {
+                    NpgsqlConnection conn4 = new NpgsqlConnection("Server=localhost;Port=5432;Database=Stacja_narciarska;User Id=postgres;Password=rowery;");
+                    conn4.Open();
 
-            NpgsqlCommand comm4 = new NpgsqlCommand();
-            comm4.Connection = conn4;
-            comm4.CommandType = CommandType.Text;
-            comm4.CommandText = "UPDATE public.cena_biletu SET cena_biletu='" + cena_bilet.Text + "' WHERE rodzaj_biletu_id_rodzaj_biletu = (SELECT id_rodzaj_biletu FROM public.rodzaj_biletu WHERE nazwa='" + lista_bilety.Text + "')";
-            comm4.ExecuteNonQuery();
-            MessageBox.Show("Zaktualizowano pomyslnie");
+                    NpgsqlCommand comm4 = new NpgsqlCommand();
+                    comm4.Connection = conn4;
+                    comm4.CommandType = CommandType.Text;
+                    comm4.CommandText = "UPDATE public.cena_biletu SET cena_biletu='" + cena_bilet.Text + "' WHERE rodzaj_biletu_id_rodzaj_biletu = (SELECT id_rodzaj_biletu FROM public.rodzaj_biletu WHERE nazwa='" + lista_bilety.Text + "')";
+                    comm4.ExecuteNonQuery();
+                    MessageBox.Show("Zaktualizowano pomyslnie");
 
-            comm4.Dispose();
-            conn4.Close();
+                    comm4.Dispose();
+                    conn4.Close();
+
+                }
+                else
+                {
+                    MessageBox.Show("Nie podano nowej ceny!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nie wybrabo biletu!");
+            }
         }
 
         private void cena_karnet_TextChanged(object sender, EventArgs e)
